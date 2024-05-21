4b24c4274d94f3ed5c1d5a517a89ad199f218493
Laurentius Nicholas <149076100+LaurentiusNicholasC@users.noreply.github.com>
2024-05-21 15:05:18 +0700

    up

---
+++ interaksi.py
@@ -0,0 +1,30 @@
+class Mahasiswa:
+
+    def __init__ (self, nama, npm):
+        self.nama = nama
+        self.npm = npm
+
+    def mengambil(self, matkul):
+        print(self.nama + ' mengambil Mata Kuliah ' + matkul.namaMatkul)
+        matkul.diambil(matkul.kapasitas, self.nama)
+
+class Mata_kuliah:
+    def __init__ (self, namaMatkul, kapasitas):
+        self.namaMatkul = namaMatkul
+        self.kapasitas = kapasitas
+
+    def diambil(self, kapasitas, nama_mahasiswa):
+        self.kapasitas -= 1
+        print ('kapasitas tersedia: ' + str(self.kapasitas))
+        print (self.namaMatkul + ' diambil oleh ' + nama_mahasiswa)
+
+PBO = Mata_kuliah('PBO', 40)
+AI  = Mata_kuliah('AI', 40)
+
+Arnora = Mahasiswa('Arnora', '2215061015')
+Nico = Mahasiswa('Nico', '2215061059')
+Theofani = Mahasiswa('Theofani', '2215061015')
+
+Arnora.mengambil(PBO)
+Nico.mengambil(AI)
+Theofani.mengambil(PBO)
